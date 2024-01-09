a23acf51bb7be94bfa9adc55a3f51b1c038565ba
PedroMarineli <pedrohenrique.marineli@gmail.com>
2024-01-09 16:30:00 +0000

    0.2 idade

--- exe.py
+++ exe.py
@@ -25,7 +25,7 @@ def escolher_opcao():
             case 1:
                 par_ou_impar()
             case 2:
-                print('2')
+                idade()
             case 3:
                 print('3')
             case 4:
@@ -55,14 +55,31 @@ def main():
 def par_ou_impar():
     exibir_titulo('Par ou ímpar')
     if int(input('\nDigite um número: ')) % 2 == 0:
-        print('O número digitado é par')
+        print('\nO número digitado é par')
     else:
-        print('O número digitado é ímpar')
+        print('\nO número digitado é ímpar')
+    voltar_ao_menu()
 
 def exibir_titulo(texto):
     os.system('clear')
     print(texto)
     print()
+
+def idade():
+    exibir_titulo('Idade')
+    idade = int(input('Digite a sua idade: '))
+    if idade > 18:
+        idade = 'adulto'
+    elif idade > 12:
+        idade = 'adolescente'
+    else:
+        idade = 'criança'
+    print(f'\nVocê é {idade}')
+    voltar_ao_menu()
+    
+def voltar_ao_menu():
+    input('\nPressione enter para voltar ao menu.')
+    main()
 
 if __name__ == '__main__':
     main()
